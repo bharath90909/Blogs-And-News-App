e1df127bb07446f65daecdfb0a3d8f280c7f78f1
Bharath Kumar <t.bharathkumarr@gmail.com>
2025-02-04 22:25:52 +0530

    Done with minor changes in blos section

--- news-and-blogs-app/src/components/MyBlogs.jsx
+++ news-and-blogs-app/src/components/MyBlogs.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import "../ui/css/MyBlogs.css";
 import { BlogsContext } from "../context/BlogsProvider";
+import { useNavigate } from "react-router-dom";
 import BlogModal from "./BlogModal";
 import BlogForm from "./BlogForm";
 function MyBlogs() {
@@ -9,6 +10,7 @@ function MyBlogs() {
   const [isBlogEditFormOpen, setIsBlogEditFormOpen] = useState(false);
   const [currentBlog, setCurrentBlog] = useState(null);
   const [currentBlogIndex, setCurrentBlogIndex] = useState(null);
+  const navigate = useNavigate();
 
   const closeBlogModal = () => {
     setIsBlogModalOpen(false);
@@ -25,52 +27,48 @@ function MyBlogs() {
   return (
     <div className="my-blogs">
       <h1 className="my-blogs-heading">My Blogs</h1>
-      {blogs.length > 0 ? (
-        <div className="blog-posts">
-          {blogs &&
-            blogs.length > 0 &&
-            blogs.map((blog, index) => {
-              return (
-                <div className="blog-post" onClick={() => showBlog(blog)}>
-                  <img src={blog.image} alt="" />
-                  <h3>{blog.title}</h3>
-                  <div className="post-buttons">
-                    <button
-                      className="edit-post"
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        setCurrentBlog(blog);
-                        setCurrentBlogIndex(index);
 
-                        setIsBlogEditFormOpen(true);
-                      }}
-                    >
-                      <i className="bx bxs-edit"></i>
-                    </button>
-                    <button
-                      className="delete-post"
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        handleDeleteBlog(blog);
-                      }}
-                    >
-                      <i className="bx bxs-x-circle"></i>
-                    </button>
-                  </div>
+      <div className="blog-posts">
+        {blogs &&
+          blogs.length > 0 &&
+          blogs.map((blog, index) => {
+            return (
+              <div className="blog-post" onClick={() => showBlog(blog)}>
+                <img src={blog.image} alt="" />
+                <h3>{blog.title}</h3>
+                <div className="post-buttons">
+                  <button
+                    className="edit-post"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setCurrentBlog(blog);
+                      setCurrentBlogIndex(index);
+
+                      setIsBlogEditFormOpen(true);
+                    }}
+                  >
+                    <i className="bx bxs-edit"></i>
+                  </button>
+                  <button
+                    className="delete-post"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleDeleteBlog(blog);
+                    }}
+                  >
+                    <i className="bx bxs-x-circle"></i>
+                  </button>
                 </div>
-              );
-            })}
-        </div>
-      ) : (
-        <p
-          className="no-blogs"
-          style={{
-            textAlign: "center",
-          }}
+              </div>
+            );
+          })}
+        <div
+          className="create-blog blog-post"
+          onClick={() => navigate("/blogs")}
         >
-          No Blogs...
-        </p>
-      )}
+          <button>+</button>
+        </div>
+      </div>
 
       {isBlogModalOpen && (
         <BlogModal currentBlog={currentBlog} closeModal={closeBlogModal} />
